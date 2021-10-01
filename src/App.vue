a1eb5131df359448aefb685c595982822f581b01
qingxin <905786329@qq.com>
2021-10-01 18:44:14 +0800

    04-添加目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-// import jsonp from 'jsonp';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
-  mounted() {
-    //第一种：axios cors跨域
-    // axios({
-    //   method:'get',
-    //   url:'http://poetry.apiopen.top/sentences'
-    // }).then((result)=>{
-    //     console.log(result.data.result)
-    // })
-
-  //第二种： jsonp跨域解决方案
-  // var url='https://www.imooc.com/search/hotwords'
-  // jsonp(url,function(err,result){
-  //   console.log(result)
-  // })
-
-  //第三种，本地服务代理 devServer
-  var url='http://localhost:8080/api1/search/hotwords'
-axios({
-  method:'get',
-  url:url
-}).then((result)=>{
-  console.log(result)
-})
-
-// axios({
-//   url:'http://localhost:8080/api2',
-//   method:'get'
-// }).then((result)=>{
-//     console.log('my qingxin'+result)
-
-// })
+  components: { 
     
   },
+  mounted() {
+
+   },
 }
 </script>
 
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <main-header></main-header>
+      <router-view></router-view>
+      <main-footer></main-footer>
+  </div>
+</template>
+
+<script>
+import MainHeader from '../components/MainHeader'
+import MainFooter from '../components/MainFooter'
+
+export default {
+    name:'main-home',
+    components:{
+        MainHeader,
+        MainFooter
+    }
+}
+</script>
+
+<style>
+
+</style>
